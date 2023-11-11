195d61fada0459b74cf12128398cc1b66a3bbba9
sofiane belabbas <sofianebelabbas93220@gmail.com>
2023-11-11 18:58:11 +0100

    message retour MODE au client

--- src/handleMode.cpp
+++ src/handleMode.cpp
@@ -20,28 +20,28 @@ int handleMode(const std::string& line, Client& client, Serveur& serveur)
 {
     std::vector<std::string> args = createArg(line);
     std::string response;
+
     
     if (args.size() < 2) 
     {
-        std::string response = client.returnPrefixe() + ERR_NEEDMOREPARAMS(args[0]) + "\r\n";
+        response = client.returnPrefixe() + ERR_NEEDMOREPARAMS(args[0]) + "\r\n";
         sendResponse(client, serveur, response);
         return 1;
-    }  
+    } 
+    std::string reason   = createReason(line, 0);
+    
     if (args[1][0] == '#' && args.size() > 2) {
         std::string channelStr = args[1];
         Channel* channel = serveur.getChannel(channelStr);
         if (channel->checkIfClientOperator(client.getNick()))
         {
-            std::string response = client.returnPrefixe() + ERR_CHANOPRIVSNEEDED(channelStr) + "\r\n";
+            response = client.returnPrefixe() + ERR_CHANOPRIVSNEEDED(channelStr) + "\r\n";
             sendResponse(client, serveur, response);
             return 1;
         }
-        if(!channel->isOperator(&client)) {
-            response = "You don't have the correct access to do this\n";
-            sendResponse(client, serveur, response);
-        }
         channel->findMode(&client, args[2], args.size() > 3 ? args[3] : "");
-        
     }
+    response = client.returnPrefixe() + "MODE #" + reason + "\r\n";
+    sendResponse(client, serveur, response);
     return (0);
 }
