b8b4322655cd5e07a01931bccfa50ea44d892d26
sofiane belabbas <sofianebelabbas93220@gmail.com>
2023-11-19 14:32:43 +0100

    INVITE Perfect

--- src/handleInvite.cpp
+++ src/handleInvite.cpp
@@ -1,9 +1,20 @@
 #include "../include/Serveur.hpp"
 
+
+
+/*
+
+args[0] = INVITE
+args[1] = said (client inviter)
+args[2] = #lol (channel name)
+
+*/
+
+
 int handleInvite(const std::string& line, Client& client, Serveur& serveur)
 {
     std::vector<std::string> args = createArg(line);
-    if (args.size() < 3) 
+    if (args.size() < 3) // nombre d'argument
     {
         std::string response = client.returnPrefixe() + ERR_NEEDMOREPARAMS(args[0]) + "\r\n";
         sendResponse(client, serveur, response);
@@ -19,7 +30,15 @@ int handleInvite(const std::string& line, Client& client, Serveur& serveur)
         sendResponse(client, serveur, response);
         return 1;
     }
-    if (channel->checkIfClientOperator(client.getNick()))
+
+    if (channel->checkIfClientInChannel(client.getNick()) == 1) // check si le client qui invite et dans le channel
+    {
+        std::string response = client.returnPrefixe() + ERR_NOTONCHANNEL(channelStr)+ "\r\n";
+        sendResponse(client, serveur, response);
+        return 1;
+    }
+
+    if (channel->checkIfClientOperator(client.getNick())) // check si le client qui invite et un operateur
     {
         std::string response = client.returnPrefixe() + ERR_CHANOPRIVSNEEDED(channelStr) + "\r\n";
         sendResponse(client, serveur, response);
@@ -27,39 +46,30 @@ int handleInvite(const std::string& line, Client& client, Serveur& serveur)
     }
 
     std::string nicknameStr = args[1];
-    if (serveur.getClient(nicknameStr) == NULL)
+    Client * targetClient = serveur.getClient(nicknameStr);
+    if (targetClient == NULL) // check si la cible existe dans le serveur
     {
         std::string response = client.returnPrefixe() + ERR_NOSUCHNICK(nicknameStr) + "\r\n";
         sendResponse(client, serveur, response);
         return 1;
     }
 
-    Client * targetClient = serveur.getClient(nicknameStr);
-    
-    if (channel)
+    if (!(channel->checkIfClientInChannel(serveur.getClient(nicknameStr)))) // check si le client inviter et deja dans le channel
     {
-        if (channel->checkIfClientInChannel(client.getNick()) == 1)
-        {
-            std::string response = client.returnPrefixe() + ERR_NOSUCHNICK(nicknameStr) + "\r\n";
-            sendResponse(client, serveur, response);
-            return 1;
-        }
-        if (!(channel->checkIfClientInChannel(serveur.getClient(nicknameStr))))
-        {
-            std::cout <<  "nickbane " << nicknameStr << std::endl;
-            std::string response = client.returnPrefixe() + ERR_USERONCHANNEL(targetClient->getNick(), channelStr) + "\r\n";
-            sendResponse(client, serveur, response);
-            return 1;
-        }
-    }
-    if (channel->getInviteStatus() == true && channel->checkIfClientOperator(&client) != 0)
-	{
-        std::string response = client.returnPrefixe() + ERR_CHANOPRIVSNEEDED(channelStr) + "\r\n";
+        std::cout <<  "nickbane " << nicknameStr << std::endl;
+        std::string response = client.returnPrefixe() + ERR_USERONCHANNEL(targetClient->getNick(), channelStr) + "\r\n";
         sendResponse(client, serveur, response);
         return 1;
     }
-    channel->addInvitedClient(targetClient);
-
+    
+    // if (channel->getInviteStatus() == true && channel->checkIfClientOperator(&client) != 0) // inutile
+	// {
+    //     std::string response = client.returnPrefixe() + ERR_CHANOPRIVSNEEDED(channelStr) + "\r\n";
+    //     sendResponse(client, serveur, response);
+    //     return 1;
+    // }
+    if (channel->checkIfClientInvited(targetClient) == 1)
+        channel->addInvitedClient(targetClient);
     std::string response = targetClient->returnPrefixe() + "INVITE " + nicknameStr + " " + channelStr + "\r\n";
     sendResponse(*targetClient, serveur, response);
     response = client.returnPrefixe() + RPL_INVITING(channelStr, nicknameStr) + "\r\n";
